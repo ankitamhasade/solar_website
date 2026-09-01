e97170b5716ccb1f256efb574d9f21006f964c91
ankitamhasade <109947523+ankitamhasade@users.noreply.github.com>
2026-09-01 20:35:13 +0530

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="4aMidwLrAVoWyFiVj-V_eBjMvNn0WDgB52UR-hhB4Jw"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
